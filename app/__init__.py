ef3680ab3eed1971a64bdfef339e396a0212abc2
Tai <taiyebabatundejames@gmail.com>
2024-07-07 21:00:13 +0100

    Last update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.config import config_by_name
+from flask_migrate import Migrate
+from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
@@ -10,21 +11,22 @@ from flask_jwt_extended import JWTManager
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+migrate = Migrate()
 
 
-def create_app(config_name):
-    
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_by_name[config_name])
+    app.config.from_object(config_class)
 
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
 
     from . import routes
     app.register_blueprint(routes.auth)
     app.register_blueprint(routes.api)
-    
+
     return app
 
 
--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app import create_app, db
-import os
 
-config_name = os.getenv('FLASK_CONFIG', 'development')
-
-app = create_app(config_name)
+app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
