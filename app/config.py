24532deaf9cb836ab663ce08a25e446ca8c0be70
Babatunde Taiye James <76518569+Tarvel@users.noreply.github.com>
2024-07-08 07:09:09 +0100

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os, datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30) 
+
