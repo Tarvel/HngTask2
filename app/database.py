fb92e2ca2fe28cbcc253017ddbb15151b58f26d1
Babatunde Taiye James <76518569+Tarvel@users.noreply.github.com>
2024-07-08 16:02:31 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,10 +2,6 @@ from app import db
 import uuid
 
 
-user_org = db.Table('user_org',
-                    db.Column('user_id', db.String, db.ForeignKey('user.id')),
-                    db.Column('org_id', db.String, db.ForeignKey('organisation.id')))
-
 class User(db.Model):
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
     firstName = db.Column(db.String, nullable=False)
@@ -14,7 +10,6 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     phone = db.Column(db.String)
     organisation = db.relationship('Organisation', secondary=user_org, backref='users')
-
 
     def __repr__(self):
         return f"User('{self.firstName}', '{self.lastName}', '{self.email}')"
@@ -26,5 +21,8 @@ class Organisation(db.Model):
     description = db.Column(db.String, default="")
 
     def __repr__(self):
-        return f"User('{self.name}')"
-    
+        return f"Organisation('{self.name}')"
+
+user_org = db.Table('user_org',
+                    db.Column('user_id', db.String, db.ForeignKey('user.id')),
+                    db.Column('org_id', db.String, db.ForeignKey('organisation.id')))
