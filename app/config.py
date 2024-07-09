f29701f74e940ed3641ffeff4646496956bf82ef
Babatunde Taiye James <76518569+Tarvel@users.noreply.github.com>
2024-07-09 14:27:33 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30) 
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
+    DEBUG = True
 
